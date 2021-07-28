0cb526927e512ec9af245ad4207d494553867a23
smartscore_smlee <smlee@smartscore.kr>
2021-07-28 18:40:04 +0900

    membership modal

--- entertainment/assets/js/modal.js
+++ entertainment/assets/js/modal.js
@@ -278,3 +278,15 @@ securitycode.addEventListener('focus', function () {
     document.querySelector('.creditcard').classList.add('flipped');
 });
 };
+
+
+const modal = document.getElementById('modal');
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal')
+})
+close.addEventListener('click', () => {
+    modal.classList.remove('show-modal')
+})
+window.addEventListener('click', (e) => {
+    e.target === modal ? modal.classList.remove('show-modal') : false
+})

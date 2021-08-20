f509846e038039ee48335ff6a0e26baa81b24e09
smartscore_smlee <smlee@smartscore.kr>
2021-08-20 17:54:24 +0900

    random animation

--- entertainment/assets/js/tag.js
+++ entertainment/assets/js/tag.js
@@ -1,12 +1,19 @@
 const tagWrap = document.querySelector('.floating-tag-wrap');
 
-for (let i=1; i<15; i++) {
+for (let i = 1; i < 15; i++) {
     const tagDiv = document.createElement("div");
     tagDiv.className = `tag tag_${i}`;
     tagWrap.appendChild(tagDiv);
     document.querySelector('.tag').innerText = getRandomName();
 }
 
+const bubbleWrap = document.querySelector('.company-audition');
+
+for(let i = 1; i < 7; i++ ) {
+    const buttonSpan = document.createElement("span")
+    buttonSpan.className = `bubble bubble_${i}`;
+    bubbleWrap.appendChild(buttonSpan);
+}
 
 
 

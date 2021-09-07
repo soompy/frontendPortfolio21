f9846ce6945556f70ffc3add72f86c327ec9e812
soompy <sooper1137@gmail.com>
2021-09-08 00:35:52 +0900

    random floating tag

--- entertainment/assets/js/tag.js
+++ entertainment/assets/js/tag.js
@@ -1,14 +1,14 @@
-const tagWrap = document.querySelector('.floating-tag-wrap');
+// const tagWrap = document.querySelector('.floating-tag-wrap');
 
-for (let i = 1; i < 15; i++) {
-    const tagDiv = document.createElement("div");
-    tagDiv.className = `tag tag_${i}`;
-    tagWrap.appendChild(tagDiv);
-    document.querySelector('.tag').innerText = getRandomName();
-}
+// for (let i = 1; i < 15; i++) {
+//     const tagDiv = document.createElement("div");
+//     tagDiv.className = `tag tag_${i}`;
+//     tagWrap.appendChild(tagDiv);
+//     document.querySelector('.tag').innerText = getRandomName();
+// }
 
-function getRandomName() {
-    var names = [
+const getRandomName = () => {
+    const names = [
         'TWICE',
         'DAY6',
         'Stray kids',
@@ -22,5 +22,22 @@ function getRandomName() {
     ];
     return names[Math.floor(Math.random() * names.length)];
 }
+
+const tagWrap = document.querySelector('.floating-tag-wrap');
+
+Array.from({ length: 15 }, (v, i) => {
+    const tagDiv = document.createElement("div");
+    tagDiv.className = `tag tag_${i}`;
+    tagDiv.innerText = getRandomName();
+    tagDiv.onanimationiteration = e => {
+        console.log(e)
+        tagDiv.innerText = `${ getRandomName() }_${e.elapsedTime}`
+    }
+    return tagDiv
+}).forEach(tag => {
+    tagWrap.appendChild(tag)
+})
+
+
 
 // document.write( getRandomName() );

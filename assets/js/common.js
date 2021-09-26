55148aca65b0d75ea9d12916da7b11e896ba4d12
soompy <sooper1137@gmail.com>
2021-09-26 23:37:24 +0900

    세부 스타일 수정

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,4 @@
-// 스크롤 시 헤더에 bg-header 클래스가 들어오도록 하기
-const scrollpos = window.scrollY
+let scrollpos = window.scrollY
 const header = document.querySelector("header")
 const header_height = header.offsetHeight
 
@@ -10,12 +9,8 @@ window.addEventListener('scroll', function() {
     scrollpos = window.scrollY;
 
     if (scrollpos > header_height) { add_class_on_scroll() }
-    else { remove_class_on_scroll() }
-
-    console.log(scrollpos)
+    else { remove_class_on_scroll() }    
 })
-
-
 
 
 
